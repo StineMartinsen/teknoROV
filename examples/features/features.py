761e9c83b68bd5f443490925250cd40ab14bcfe7
StineMartinsen <80045646+StineMartinsen@users.noreply.github.com>
2022-06-16 15:17:01 +0200

    la til GPIOene

--- examples/features/features.py
+++ examples/features/features.py
@@ -1,12 +1,20 @@
 import os
 import subprocess
+from tkinter import RIGHT
 
 import Pyro4
 
 from edurov import WebMethod
 
 
+import RPi.GPIO as GPIO
 
+#set GPIO numbering mode and define output pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.OUT) #UO
+GPIO.setup(5,GPIO.OUT) #DOWN
+GPIO.setup(6,GPIO.OUT) #RIGTH
+GPIO.setup(26,GPIO.OUT) #LEFT
 
 
 def control_motors():
@@ -16,13 +24,29 @@ def control_motors():
             while rov.run:
                 if keys.state('K_UP'):
                     print('Forward')
-                elif keys.state('K_DOWN'):
+                    UP = True
+                else:
+                    UP = False
+                if keys.state('K_DOWN'):
                     print('Backward')
-                elif keys.state('K_RIGHT'):
+                    DOWN = True
+                else:
+                    DOWN = False
+                if keys.state('K_RIGHT'):
                     print('Right')
-                elif keys.state('K_LEFT'):
+                    RIGHT = True
+                else:
+                    RIGTH = False
+                if keys.state('K_LEFT'):
                     print('left')
+                    LEFT = True
+                else:
+                    LEFT = False
 
+                GPIO.output(4,UP)
+                GPIO.output(5,DOWN)
+                GPIO.output(6,RIGTH)
+                GPIO.output(26,LEFT)
 
 # Create the WebMethod class
 web_method = WebMethod(
